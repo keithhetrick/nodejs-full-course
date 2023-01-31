95353dea33cb9dc15fb6290bc9e29dfdd5367f7d
Keith Hetrick <keithjhetrick@gmail.com>
2023-01-31 15:54:25 -0600

    chapter 13 added

---
+++ Chapter14/config/dbConn.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    mongoose.set("strictQuery", true);
+    const conn = await mongoose.connect(process.env.DATABASE_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`\nMongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error("\nERROR:", error.message, "\n");
+    console.error(error.stack);
+    console.error("\nExiting process...\n");
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
---
+++ Chapter14/middleware/verifyRoles.js
@@ -0,0 +1,16 @@
+const verifyRoles = (...allowedRoles) => {
+  return (req, res, next) => {
+    if (!req?.roles) return res.sendStatus(401); //Unauthorized
+    const rolesArray = [...allowedRoles];
+    console.log("\nRoles Array:", rolesArray);
+    console.log("\nUser Roles:", req.roles);
+    const result = req.roles
+      .map((role) => rolesArray.includes(role))
+      .find((val) => val === true);
+
+    if (!result) return res.sendStatus(403); //Forbidden
+    next();
+  };
+};
+
+module.exports = verifyRoles;
